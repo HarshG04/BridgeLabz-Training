8cf697b1a7b7065f660827a0790c1aa4dcc4d2e2
Harsh Goyal <goyalharsh502@gmail.com>
2026-01-07 15:33:39 +0530

    [Harsh] .Add Added EmployeeWageComputation Version-1

---
+++ oops-csharp-practice/scenario-based/EmployeeWage/EmployeeWage/IEmployee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal interface IEmployee
+    {
+        void AddEmployee();
+        void DisplayEmployee(Employee employee);
+
+        // UC-1
+        void CheckAttandance();
+    }
+}
