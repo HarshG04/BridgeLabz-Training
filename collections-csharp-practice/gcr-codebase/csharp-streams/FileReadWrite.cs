88ba5eed2a3cfedfa331fba08e541c973a75d932
Harsh Goyal <goyalharsh502@gmail.com>
2026-01-22 22:37:03 +0530

    [Harsh] Add .Added csharp-streams code files

---
+++ collections-csharp-practice/gcr-codebase/csharp-streams/FileReadWrite.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+class FileCopy
+{
+    static void Main()
+    {
+        string sourcePath = "sampleFile.txt";
+        string destinationPath = "destination.txt";
+
+        try
+        {
+            if (!File.Exists(sourcePath))
+            {
+                Console.WriteLine("Source file does not exist.");
+                return;
+            }
+
+            using (FileStream sourceStream = new FileStream(sourcePath, FileMode.Open, FileAccess.Read))
+            using (FileStream destStream = new FileStream(destinationPath, FileMode.Create, FileAccess.Write))
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+
+                while ((bytesRead = sourceStream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    destStream.Write(buffer, 0, bytesRead);
+                }
+            }
+
+            Console.WriteLine("File copied successfully.");
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine("I/O Error: " + ex.Message);
+        }
+    }
+}
